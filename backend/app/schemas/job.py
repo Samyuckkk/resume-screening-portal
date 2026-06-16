7dac1d6a78363242da2d28d2ba9372a3ab7d4d9c
Samyuckkk <samyakoholkar@gmail.com>
2026-06-16 09:46:27 +0530

    Job Schema Fixed

--- backend/app/schemas/job.py
+++ backend/app/schemas/job.py
@@ -1,23 +1,20 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
-class UserRegister(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-    role: str
+class JobCreate(BaseModel):
+    title: str
+    description: str
+    location: str
+    salary: int
 
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
-
-class UserResponse(BaseModel):
+class JobResponse(BaseModel):
     id: int
-    name: str
-    email: EmailStr
-    role: str
+    title: str
+    description: str
+    location: str
+    salary: int
+    recruiter_id: int
 
     class Config:
         from_attributes = True
